c8c62ece28361b0f13606c23533fb1cb6d23b5b1
yubin <nrbnov28@gmail.com>
2024-11-05 21:57:27 +0900

    마무리

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -14,5 +14,5 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findAllByUser(User user, Pageable pageable);
 
-    Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
+    Page<Product> findAllByUserAndProductFolderList_FolderId(User user, java.lang.Long folderId, Pageable pageable);
 }
